ca797131e489119d42bd26ddd0f06aeb45116cb2
Eugenio Paolantonio (g7) <me@medesimo.eu>
2015-07-07 00:11:41 +0200

    Initial commit.
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

---
+++ modules/updates/updates.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+#
+# updates - Semplice update preferences
+# Copyright (C) 2015  Eugenio "g7" Paolantonio
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Authors:
+#    Eugenio "g7" Paolantonio <me@medesimo.eu>
+#
+
+from veracc.widgets.UnlockBar import UnlockBar, ActionResponse
+from gi.repository import Gio, GObject, Gtk
+
+import quickstart
+
+# As this is a separate VeraCC module, we need to load translations separately...
+TRANSLATION = quickstart.translations.Translation("vera-control-center-module-updates")
+TRANSLATION.load()
+TRANSLATION.bind_also_locale()
+
+_ = TRANSLATION._
+
+BASE_PROVIDER = "semplice-base.provider"
+CURRENT_CHANNEL = "semplice-current"
+PROPOSED_COMPONENT = "proposed"
+DEVELOPMENT_CHANNEL = "semplice-devel"
+
+BUS_NAME = "org.semplicelinux.channels"
+
+@quickstart.builder.from_file("./modules/updates/updates.glade")
+class Scene(quickstart.scenes.BaseScene):
+	""" Desktop preferences. """
+	
+	events = {
+		"changed" : ["selected_channel"],
+		"toggled" : ["enable_proposed_updates", "enable_development_updates"]
+	}
+	
+	# Used to define the currently-enabled semplice-base channel. 
+	base_channel_enabled = None
+	
+	building = False
+	
+	def on_scene_asked_to_close(self):
+		"""
+		Do some cleanup
+		"""
+		
+		self.bus_cancellable.cancel()
+		
+		return True
+	
+	def prepare_scene(self):
+		"""
+		Fired when doing the scene setup.
+		"""
+				
+		self.scene_container = self.objects.main
+
+		# Create unlockbar
+		self.unlockbar = UnlockBar("org.semplicelinux.channels.manage")
+		self.objects.main.pack_start(self.unlockbar, False, False, 0)
+
+		# Set-up channel combobox
+		renderer = Gtk.CellRendererText()
+		self.objects.selected_channel.pack_start(renderer, True)
+		self.objects.selected_channel.add_attribute(renderer, "text", 1)
+		
+		# Ensure that the updates_frame is not sensitive when the settings
+		# are locked...
+		self.unlockbar.bind_property(
+			"lock",
+			self.objects.updates_frame,
+			"sensitive",
+			GObject.BindingFlags.INVERT_BOOLEAN
+		)
+
+	def on_selected_channel_changed(self, combobox):
+		"""
+		Fired when the selected channel has been changed.
+		"""
+		
+		if self.building:
+			return
+		
+		new_channel = self.objects.semplice_base_channels.get_value(combobox.get_active_iter(), 0)
+		
+		self.Channels.Enable("(s)", new_channel)
+		
+		# Store the new choice
+		self.base_channel_enabled = new_channel
+		
+		# Check for features
+		self.check_for_features()
+		
+
+	def on_enable_proposed_updates_toggled(self, checkbutton):
+		"""
+		Fired when the Enable proposed updates checkbutton has been toggled.
+		"""
+		
+		if self.building:
+			return
+		
+		(self.Channels.EnableComponent if checkbutton.get_active() else self.Channels.DisableComponent)("(ss)", self.base_channel_enabled, PROPOSED_COMPONENT)
+	
+	def on_enable_development_updates_toggled(self, checkbutton):
+		"""
+		Fired when the Enable development updates checkbutton has been toggled.
+		"""
+		
+		if self.building:
+			return
+		
+		(self.Channels.Enable if checkbutton.get_active() else self.Channels.Disable)("(s)", DEVELOPMENT_CHANNEL)
+
+	@quickstart.threads.on_idle
+	def check_for_features(self):
+		"""
+		"Enable proposed updates" and "Enable development updates" are
+		available only on semplice-current.
+		
+		This method ensures that those checkbuttons are not sensitive if
+		the selected base channel is not semplice-current and loads their
+		settings if it is.
+		"""
+		
+		self.building = True
+		
+		if self.base_channel_enabled == CURRENT_CHANNEL:			
+			self.objects.enable_development_updates.set_sensitive(True)
+			if self.Channels.GetEnabled("(s)", DEVELOPMENT_CHANNEL):
+				self.objects.enable_development_updates.set_active(True)
+			else:
+				self.objects.enable_development_updates.set_active(False)
+		else:
+			self.objects.enable_development_updates.set_sensitive(False)
+			self.objects.enable_development_updates.set_active(False)
+
+		# Check if the "proposed" channel is available
+		if self.Channels.HasComponent("(ss)", self.base_channel_enabled, PROPOSED_COMPONENT):
+			self.objects.enable_proposed_updates.set_sensitive(True)
+			
+			if self.Channels.GetComponentEnabled("(ss)", self.base_channel_enabled, PROPOSED_COMPONENT):
+				self.objects.enable_proposed_updates.set_active(True)
+			else:
+				self.objects.enable_proposed_updates.set_active(False)
+		else:
+			self.objects.enable_proposed_updates.set_sensitive(False)
+			self.objects.enable_proposed_updates.set_active(False)
+		
+		self.building = False
+		
+	@quickstart.threads.on_idle
+	def load(self):
+		"""
+		Reloads the semplice_base_channels ListStore.
+		"""
+		
+		self.building = True
+		
+		self.objects.semplice_base_channels.clear()
+		
+		for channel in self.Providers.WhatProvides("(s)", BASE_PROVIDER):
+			details = self.Channels.GetDetails("(sas)", channel, ["name"])
+			itr = self.objects.semplice_base_channels.append(
+				[
+					channel,
+					details["name"] if "name" in details else channel
+				]
+			)
+			
+			if self.Channels.GetEnabled("(s)", channel):
+				self.objects.selected_channel.set_active_iter(itr)
+				self.base_channel_enabled = channel
+		
+		self.check_for_features()
+		
+		# self.building will be restored by check_for_features()
+
+	def on_scene_called(self):
+		"""
+		Fired when the scene has been called.
+		"""
+
+		# Enter in the bus
+		self.bus_cancellable = Gio.Cancellable()
+		self.bus = Gio.bus_get_sync(Gio.BusType.SYSTEM, self.bus_cancellable)
+		self.Channels = Gio.DBusProxy.new_sync(
+			self.bus,
+			0,
+			None,
+			BUS_NAME,
+			"/org/semplicelinux/channels/channels",
+			"org.semplicelinux.channels.channels",
+			self.bus_cancellable
+		)
+		self.Providers = Gio.DBusProxy.new_sync(
+			self.bus,
+			0,
+			None,
+			BUS_NAME,
+			"/org/semplicelinux/channels/providers",
+			"org.semplicelinux.channels.providers",
+			self.bus_cancellable
+		)
+
+		# We are locked
+		self.unlockbar.emit("locked")
+		
+		self.load()
---
+++ setup.py
@@ -0,0 +1,194 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# updates - Semplice update preferences
+# Copyright (C) 2015  Eugenio "g7" Paolantonio
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Authors:
+#    Eugenio "g7" Paolantonio <me@medesimo.eu>
+#
+
+import os
+from distutils.core import setup, Command
+from distutils.command.build import build
+from distutils.command.install import install
+
+import subprocess
+import shutil
+
+l10n_path = "./po"
+
+APP_NAME = "vera-control-center-module-updates"
+
+
+class CreatePotTemplate(Command):
+	"""
+	Creates a .pot template.
+	"""
+	
+	description = "creates a .pot localization template from the program sources."
+	user_options = []
+	
+	def initialize_options(self):
+		"""
+		Initialize options
+		"""
+		
+		self.cwd = None
+	
+	def finalize_options(self):
+		"""
+		Finalize options
+		"""
+		
+		self.cwd = os.getcwd()
+	
+	def run(self):
+		"""
+		Does things.
+		"""
+		
+		assert os.getcwd() == self.cwd, "You must be in the package root: %s" % self.cwd
+		
+		output_file = os.path.join(self.cwd, l10n_path, APP_NAME, "%s.pot" % APP_NAME)
+		
+		# Blank-out the output file
+		with open(output_file, "w") as f:
+			f.write("")
+		
+		py_files = []
+		glade_files = []
+		
+		for directory, dirnames, filenames in os.walk("."):
+			for file_ in filenames:
+				if file_.endswith(".py"):
+					py_files.append(os.path.join(directory, file_))
+				elif file_.endswith(".glade"):
+					glade_files.append(os.path.join(directory, file_))
+					
+		subprocess.call([
+			"xgettext",
+			"--language=Python",
+			"--from-code=utf-8",
+			"--keyword=_",
+			"--output=%s" % output_file,
+		] + py_files)
+		
+		for file_ in glade_files:
+			subprocess.call([
+				"intltool-extract",
+				"--type=gettext/glade",
+				file_
+			])
+			subprocess.call([
+				"xgettext",
+				"--from-code=utf-8",
+				"--language=C",
+				"--keyword=N_",
+				"-j",
+				"--output=%s" % output_file,
+				"-j",
+				file_ + ".h"
+			])
+			os.remove(file_ + ".h")
+
+class CustomBuild(build):
+	"""
+	Hooks.
+	"""
+	
+	def run(self):
+		"""
+		Runs the installation.
+		"""
+		
+		super().run()
+		
+		# Build mos
+		for directory, dirnames, filenames in os.walk(l10n_path):
+			for file_ in filenames:
+				if file_.endswith(".po"):
+					source = os.path.join(directory, file_)
+					target_dir = os.path.join("./build", directory)
+					target = os.path.join(target_dir, file_.replace(".po",".mo"))
+					
+					if not os.path.exists(target_dir):
+						os.makedirs(target_dir)
+					
+					print("Compiling translation %s" % file_)
+					subprocess.call(["msgfmt", "--output-file=%s" % target, source])
+
+class CustomInstall(install):
+	"""
+	Hooks.
+	"""
+	
+	def run(self):
+		"""
+		Runs the installation.
+		"""
+		
+		super().run()
+		
+		# Install mos
+		for directory, dirnames, filenames in os.walk(os.path.join("./build", l10n_path)):
+			for file_ in filenames:
+				if file_.endswith(".mo"):
+					source = os.path.join(directory, file_)
+					target_dir = os.path.join(
+						self.root if self.root else "/",
+						"usr/share/locale",
+						file_.replace(".mo",""),
+						"LC_MESSAGES"
+					)
+					target = os.path.join(target_dir, os.path.basename(directory) + ".mo")
+					
+					if not os.path.exists(target_dir):
+						os.makedirs(target_dir)
+					
+					shutil.copyfile(source, target)
+					os.chmod(target, 644)
+
+setup(
+	cmdclass={
+		"pot": CreatePotTemplate,
+		"build": CustomBuild,
+		"install": CustomInstall
+	},
+	name=APP_NAME,
+	version='0.1.0',
+	description='Semplice update preferences',
+	author='Eugenio Paolantonio',
+	author_email='me@medesimo.eu',
+	url='https://github.com/semplice/vera-control-center-module-updates',
+	packages=[
+		'modules',
+		'modules.updates'
+	],
+	data_files=[
+		(
+			"/usr/share/vera-control-center/modules/updates",
+			[
+				"modules/updates/updates.desktop",
+				"modules/updates/updates.glade"
+			]
+		)
+	],
+	requires=[
+		'gi.repository.Gio',
+		'gi.repository.GObject',
+		'gi.repository.Gtk',
+	]
+)

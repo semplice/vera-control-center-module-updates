89275ff454d646e90be972c1e4495af19fd7add2
Eugenio Paolantonio (g7) <me@medesimo.eu>
2015-12-15 18:27:59 +0100

    0.70.0-1 (devel)
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- setup.py
+++ setup.py
@@ -179,7 +179,7 @@ setup(
 		"install": CustomInstall
 	},
 	name=APP_NAME,
-	version='0.1.1',
+	version='0.70.0',
 	description='Semplice update preferences',
 	author='Eugenio Paolantonio',
 	author_email='me@medesimo.eu',
